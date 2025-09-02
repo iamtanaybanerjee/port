e82e4f81a18e7e41ade5d5756784b5f82f306ce9
Tanay Banerjee <tanaybanerjee@Tanays-MacBook-Air.local>
2025-09-02 17:18:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// serve static files from public
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ models/Track.model.js
@@ -0,0 +1,3 @@
+module.exports = (sequelize, DataTypes) => {
+  const Track = sequelize.define("Track", {});
+};
